3e8ee3e82046a3646a76406f3ae3774b6b6ceb0c
Oleg Tarasov <tolegu@ya.ru>
2021-12-09 20:50:17 +0300

    echobot for VK

--- bot_vk.py
+++ bot_vk.py
@@ -1,4 +1,5 @@
 import logging
+import random
 from environs import Env
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
@@ -51,6 +52,14 @@ def get_dialog_flow_answer(update, context) -> None:
     update.message.reply_text(answer)
 
 
+def echo(event, vk_session_api):
+    vk_session_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1,1000)
+    )
+
+
 def main() -> None:
     """Start the bot."""
     env = Env()
@@ -60,16 +69,11 @@ def main() -> None:
 
     vk_session = vk_api.VkApi(token=VK_TOKEN)
 
+    vk_session_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
-
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_session_api)
 
 
 if __name__ == '__main__':
